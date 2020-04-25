8947f267ecfc5366155ca0cbe7a3663bd3b5b526
Enamul121 <enamul172@gmail.com>
2020-04-25 16:25:14 +0600

    Initial commit

---
+++ src/main/java/com/app/SpringProfile101Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfile101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProfile101Application.class, args);
+    }
+
+}
